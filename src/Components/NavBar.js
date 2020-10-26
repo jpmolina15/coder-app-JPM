1f7d65effbb3ecb59f994fb7291aec2235440387
jpmolina15 <jpm15@MacBook-Pro.local>
2020-10-26 20:01:06 -0300

    NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import react, { Component } from 'react';
+
+class NavBar extends Component {
+    render () {
+        return (
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        )
+    }
+};
+
+export default NavBar
