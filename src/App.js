91bd81124cfd53e1ce1a33512de71ed16ce1313e
jpmolina15 <jpm15@MacBook-Pro.local>
2020-11-03 19:04:48 -0300

    home greeting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar'
+import Home from './Containers/Home';
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Home greetings="Hola Bienvenido" />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Containers/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const home = (props) => {
+    return (
+        <div>
+            {props.greetings}
+        </div>)
+
+}
+
+
+export default home
