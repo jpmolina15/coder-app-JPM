4f7c90f006cb196dcdbc7967ff117d8b9c932f8a
jpmolina15 <jpm15@MacBook-Pro.local>
2020-11-03 23:22:39 -0300

    a

--- src/Components/CartIcon/Carticon.js
+++ src/Components/CartIcon/Carticon.js
@@ -1,4 +1,8 @@
 import React from 'react';
-import icono from '../assets/peli.png'
+import icono from '../../assets/peli.png';
+import './CartIcon.css'
+
+
 const cartIcon = () => <img src={icono} alt="icono" /> 
+
 export default cartIcon
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import './NavBar.css'
-import CartIcon from '../Carticon'
+import CartIcon from '../CartIcon/Carticon'
 
 
 class NavBar extends Component {
     render() {
         return (
             <div>
-                <ul id='button' class='navbar'> 
-                    <li id='icono'><CartIcon/></li>
-                    <li>Sobre nosotros</li>
-                    <li>Servicios</li>
-                    <li>Productos</li>
-                    <li>Contacto</li>
+                <ul id='button' className='navbar'>
+                    <li ><CartIcon className='icono' /></li>
+                    <li><a>Sobre nosotros</a></li>
+                    <li><a>Servicios</a></li>
+                    <li><a>Productos</a></li>
+                    <li><a>Contacto</a></li>
                 </ul>
             </div>
         )
